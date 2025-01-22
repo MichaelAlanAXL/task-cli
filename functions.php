eee76c4df1471a35e06669c36b95942cc0634f3d
MichaelAlanAXL <michael.dtna@gmail.com>
2025-01-22 00:02:01 -0300

    tarefas de add, list, done and delete adicionadas

---
+++ functions.php
@@ -0,0 +1,82 @@
+<?php 
+date_default_timezone_set('America/Sao_Paulo');
+
+define('TASKS_FILE', 'tasks.json');
+
+function addTask($taskDescription) {    
+    $tasks = loadTasks();
+
+    $newTask = [
+        'id'=> count($tasks) + 1,
+        'task' => $taskDescription,
+        'status' => 'pendente',
+        'create_at' => date('d-m-Y H:i:s'),
+        'update_at' => date('d-m-Y H:i:s')
+    ];
+
+    $tasks[] = $newTask;
+    saveTasks($tasks);
+
+    echo "Tarefa adicionada com sucesso: {$taskDescription}\n";
+}
+
+function listTasks() {
+    $tasks = loadTasks();
+
+    if (empty($tasks)) {
+        echo "Nenhuma tarefa encontrada.\n";
+    } else {
+        foreach ($tasks as $task) {
+            echo "[{$task['id']}] - {$task['task']} ({$task['status']}) - {$task['create_at']}\n";
+        }
+    }
+}
+
+function markTaskDone($taskId) {
+        $tasks = loadTasks();
+
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $taskId) {
+                $task['status'] = 'concluída';
+                saveTasks($tasks);
+                echo "Tarefa ID {$taskId} marcada como concluída.\n";
+                return;
+            }
+        }
+
+        echo "Tarefa com ID {$taskId} não encontrada.\n";
+    }
+
+function loadTasks() {
+    if (!file_exists(TASKS_FILE) || filesize(TASKS_FILE) == 0) {
+        return [];
+    }
+    return json_decode(file_get_contents(TASKS_FILE), true) ?? [];
+}
+
+function saveTasks($tasks) {
+    file_put_contents(TASKS_FILE, json_encode($tasks, JSON_PRETTY_PRINT));
+}
+
+function delete($taskId) {
+    $tasks = loadTasks();
+    $taskFound = false;
+
+    foreach ($tasks as $key => $task) {
+        if ($task['id'] === $taskId) {
+            unset($tasks[$key]);
+            $taskFound = true;
+            break;
+        }
+    }
+
+    if ($taskFound) {
+        saveTasks($tasks);
+        echo "Tarefa com ID {$taskId} excluída com sucesso.\n" ;
+    } else {
+        echo "Tarefa com ID {$taskId} não encontrada.\n";
+    }
+
+}
+
+?>
--- todolist.php
+++ todolist.php
@@ -1,23 +1,49 @@
 <?php 
 
-// Pega os dados passados no terminal
+require_once 'functions.php';
+
 $arguments = $_SERVER['argv'];
 
-// Verificação
 if (isset($arguments[1])) {
 	$command = $arguments[1];
 
-	if ($command === 'hello') {
-		echo "Hello, World!\n";
-	} else {
-		echo "Comando não reconhecido. Tente: php todolist.php hello\n";
+	switch ($command) {
+		case 'add':
+			if (isset($arguments[2])) {
+				addTask($arguments[2]);
+			} else {
+				echo "Erro: Informe uma descrição para a tarefa.\n";
+			}
+			break;
+
+		case 'list':
+			listTasks();
+			break;
+
+		case 'done':
+			if (isset($arguments[2])) {
+				$taskId = (int)$arguments[2];
+				markTaskDone($taskId);
+			} else {
+				echo "Erro: Informe o ID da tarefa a ser concluída.\n";
+			}
+			break;
+
+		case 'delete':
+			if (isset($arguments[2])) {
+				$taskId = (int)$arguments[2];
+				delete($taskId);
+			} else {
+				echo "ID da tarefa não fornecido.\n";
+			}
+			break;
+
+		default:
+			echo "Comando não reconhecido. Use: add, list, done, delete\n";
+			break;
 	}
 } else {
-	echo "Nenhum comando fornecido. Tente: php todolist.php hello\n";
+	echo "Uso: php todolist.php [comando] [argumentos]\n";
 }
 
-
-
-
-
- ?>
+?>
